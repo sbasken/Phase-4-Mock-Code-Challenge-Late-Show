a43f8182e244589ae2ab72e12a49cbd2603db4d2
Saki <sbasken@gmail.com>
2023-04-12 10:42:18 -0700

    Complete deliverables

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,22 +1,65 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 
-from models import db, Episode
+from models import db, Episode, Guest, Appearance
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
-    return ''
+    return f'<h1>Welcome to Late Show!<h1>'
 
+@app.route('/episodes', methods=['GET'])
+def episodes():
+    if request.method == 'GET':
+        all_episodes = [ ep.to_dict() for ep in Episode.query.all() ]
+        return make_response( all_episodes, 200)
+    
+@app.route('/episodes/<int:id>', methods=['GET', 'DELETE'])
+def episode_by_id(id):
+    found_ep = Episode.query.filter(Episode.id == id).first()
+    if not found_ep:
+        return make_response({"error": "404: Episode not found"}, 404)
+    elif request.method == 'GET':
+        return make_response(found_ep.to_dict(rules=('guests.name','guests.id', 'guests.occupation')), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(found_ep)
+        db.session.commit()
+        return make_response({}, 204)
+    
+@app.route('/guests', methods=['GET'])
+def guests():
+    if request.method == 'GET':
+        all_guests = [ gu.to_dict() for gu in Guest.query.all() ]
+        return make_response(all_guests, 200)
+    
+@app.route('/appearances', methods=['POST'])
+def appearances():
+    data = request.get_json()
+    try:
+        new_appearance = Appearance(
+            rating = data['rating'],
+            episode_id = data['episode_id'],
+            guest_id = data['guest_id']
+        )
+        db.session.add(new_appearance)
+        db.session.commit()
+        return make_response(new_appearance.to_dict(rules=('episode', 'guest', '-episode_id', '-guest.id')), 201)
+    except ValueError:
+        return make_response({ "error": "400: Validation error."}, 400)
+    
+    
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,10 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Episode(db.Model):
-    __tablename__ = 'Episode'
+db = SQLAlchemy(metadata=metadata)
+
+class Episode( db.Model, SerializerMixin ):
+    __tablename__ = 'episodes'
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    serialize_rules = ('-created_at', '-updated_at', '-appearances')
+    
+    appearances = db.relationship('Appearance', back_populates='episode')
+    guests = association_proxy('appearances', 'guest')
+
+    def __repr__(self):
+        return f'<ID: {self.id}, Date: {self.date}, Number: {self.number}>'
+    
+class Appearance( db.Model, SerializerMixin ):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+
+    episode = db.relationship('Episode', back_populates='appearances')
+    guest = db.relationship('Guest', back_populates='appearances')
+
+    serialize_rules = ('-created_at', '-updated_at', '-episode', '-guest')
+
+    @validates('rating')
+    def validates_rating(self, key, rating):
+        if not 1 <= rating <= 5:
+            raise ValueError("Rating must be between 1 and 5.")
+
+    def __repr__(self):
+        return f'<ID: {self.id}, Rating: {self.rating}>'
+
+class Guest( db.Model, SerializerMixin ):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', back_populates='guest')
+    episodes = association_proxy('appearances', 'episode')
+    
+    serialize_rules = ('-created_at', '-updated_at', '-appearances')
+
+    def __repr__(self):
+        return f'<ID: {self.id}, Name: {self.name}, Occupation: {self.occupation}>'
+    
+
+    
 
 # add any models you may need. 
